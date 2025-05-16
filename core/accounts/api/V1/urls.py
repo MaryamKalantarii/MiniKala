2a709c0733fd555fe9bc697f811c952b8770c43f
MaryamKalantarii <maryamkalantaria72@gmail.com>
2025-05-16 12:27:25 +0330

    change-password url

--- core/accounts/api/V1/urls.py
+++ core/accounts/api/V1/urls.py
@@ -3,6 +3,7 @@ from .views import (
     RegistrationView,
     IsVerifiedView,
     ResendEmailView,
+    ChangePasswordView,
     PasswordResetRequestView,
     PasswordResetConfirmView,
 
@@ -16,10 +17,11 @@ from rest_framework_simplejwt.views import (
 app_name='api-v1-accounts'
 
 urlpatterns = [
-    
+
     path('registration/', RegistrationView.as_view(), name='registration'),
     path("is-verified/<str:token>", IsVerifiedView.as_view(), name="is-verified"),
     path("resend/", ResendEmailView.as_view(), name="resend"),
+    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
     path("reset-password/", PasswordResetRequestView.as_view(), name="reset-password"),
     path("reset-password-confirm/<str:token>/", PasswordResetConfirmView.as_view(), name="reset-password-confirm"),
 
